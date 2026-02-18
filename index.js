50478a5ae75d7dbc49ec76119eb283729ce0e7b9
themadman1219 <norbertstepak@gmail.com>
2026-02-18 13:06:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+io.on("disconnect", () => {
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
